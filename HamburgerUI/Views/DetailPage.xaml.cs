3d01797b34e2ebcbb8ec7ee111ba958bf9ec01b9
McFunston <mica.funston@gmail.com>
2016-12-17 17:18:00 -0500

    Initial

---
+++ HamburgerUI/Views/DetailPage.xaml.cs
@@ -0,0 +1,16 @@
+using HamburgerUI.ViewModels;
+using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Controls;
+
+namespace HamburgerUI.Views
+{
+    public sealed partial class DetailPage : Page
+    {
+        public DetailPage()
+        {
+            InitializeComponent();
+            NavigationCacheMode = NavigationCacheMode.Disabled;
+        }
+    }
+}
+
