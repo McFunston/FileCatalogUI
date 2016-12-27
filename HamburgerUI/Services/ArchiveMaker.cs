6461db414145cd6f938c33505c9522c87344bd9d
McFunston <mica.funston@gmail.com>
2016-12-27 12:03:54 -0500

    Clean up unusable properties from Archive. Create ArchiveMaker static Class.

---
+++ HamburgerUI/Services/ArchiveMaker.cs
@@ -0,0 +1,20 @@
+﻿using HamburgerUI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HamburgerUI.Services
+{
+    public static class ArchiveMaker
+    {
+        public static async Task<Archive> MakeNewArchiveAsync(string name, string path)
+        {
+            Archive archive = new Archive(name);
+            var fileList = await FileListFactory.GetFileListFromPathAsync(path);
+            archive.Files = fileList.FileList;
+            return archive;
+        }
+    }
+}
