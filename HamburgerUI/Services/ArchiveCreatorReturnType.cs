46159b578518b3bbf05fe31b23bdd4ad1b75cc7c
McFunston <mica.funston@gmail.com>
2016-12-27 12:56:30 -0500

    Change MakeNewArchiveAsync's return type to ArchiveCreatorReturnType.

---
+++ HamburgerUI/Services/ArchiveCreatorReturnType.cs
@@ -0,0 +1,16 @@
+﻿using HamburgerUI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HamburgerUI.Services
+{
+    public class ArchiveCreatorReturnType
+    {
+        public bool Success { get; set; }
+        public Archive archive { get; set; }
+        public string ErrorMessage { get; set; }
+    }
+}
--- HamburgerUI/Services/ArchiveMaker.cs
+++ HamburgerUI/Services/ArchiveMaker.cs
@@ -9,11 +9,13 @@ namespace HamburgerUI.Services
 {
     public static class ArchiveMaker
     {
-        public static async Task<Archive> MakeNewArchiveAsync(string name, string path)
+        public static async Task<ArchiveCreatorReturnType> MakeNewArchiveAsync(string name, string path)
         {
-            Archive archive = new Archive(name);
+            ArchiveCreatorReturnType archive = new ArchiveCreatorReturnType();
+            archive.archive.Name = name;
             var fileList = await FileListFactory.GetFileListFromPathAsync(path);
-            archive.Files = fileList.FileList;
+            archive.archive.Files = fileList.FileList;
+            archive.ErrorMessage = fileList.ErrorMessage;
             return archive;
         }
     }
